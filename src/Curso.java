7fe8557dc53af2c626b22a9c65b65a4e8356e37a
Jean Pierre <jean.pierre988@hotmail.com>
2020-12-03 08:05:20 -0300

    Working with Optional and LocalDate
    Some exercises with Optional and stream together
    Learning about LocalDate

--- src/Curso.java
+++ src/Curso.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Curso {
     private String nome;
@@ -22,7 +24,7 @@ public class Curso {
 
 class Testa {
     public static void main(String[] args) {
-        List<Curso> cursos = new ArrayList<Curso>();
+        List<Curso> cursos = new ArrayList<>();
         cursos.add(new Curso("Python", 45));
         cursos.add(new Curso("JavaScript", 150));
         cursos.add(new Curso("Java 8", 113));
@@ -31,8 +33,19 @@ class Testa {
 //        cursos.sort(Comparator.comparing(Curso::getAlunos));
 //        cursos.forEach(al -> System.out.println(al.getAlunos()));
 
+//        cursos.stream()
+//                .filter(curso -> curso.getAlunos() >= 150)
+//                .findAny()
+//                .ifPresent(curso -> System.out.println(curso.getNome()));
+
+        List<Curso> cursoList = new ArrayList<>();
         cursos.stream()
-                .map(Curso::getAlunos)
-                .forEach(System.out::println);
+                .filter(curso -> curso.getAlunos() > 50)
+                .findFirst();
+
+        cursoList.forEach(Curso::getNome);
+
+//        Curso cursoOptional = any.orElse(null);
+//        any.ifPresent(curso -> System.out.println(curso.getNome()));
     }
 }
